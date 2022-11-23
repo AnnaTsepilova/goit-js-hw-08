ea1338e5b0636cc6b64bae080149a5599f242b95
Anna Tsepilova <virgo.tsav@gmail.com>
2022-11-23 20:50:01 +0200

    fixed package.json

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+player.on('play', function() {
+ 
+});
+
+player.getVideoTitle().then(function(title) {
+    
+});
+
+
+const onPlay = function (data) {
+    console.log(data);
+};
+
+player.on('timeupdate', onPlay);
